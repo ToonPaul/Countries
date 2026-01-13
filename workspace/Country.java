2ee27acb1827db7546a6b71b193c913f588d1527
Toon Paul <hudallap@gmail.com>
2026-01-13 19:11:20 +0000

    constructor

--- workspace/Country.java
+++ workspace/Country.java
@@ -1,14 +1,43 @@
-public class Country
-{
-  // add private instance variables for the name, capital, language, and image file.
+public class Country {
+  // add private instance variables for the name, capital, language, and image
+  // file.
+  private String name;
+  private String capital;
+  private String language;
+  private String imageFile;
 
   // add constructors
+public country(String couName, String couCaptial, String conLan, String couImg){
+  name = couName;
+  capital = couCaptial;
+  language = conLan;
+  imageFile = couImg;
+}
 
   // Write accessor/get methods for each instance variable that returns it.
+  public String getName() {
+    return name;
+  }
 
-  // Write a toString() method that returns a concatenated String of 3 of the instance variables in a sentence like "..'s capital is .. and its primary language is ..."
- 
+  public String getCapital() {
+    return capital;
+  }
 
+  public String getLan() {
+    return language;
+  }
 
-  
+  public String getImage() {
+    return imageFile;
+  }
+
+  // Write a toString() method that returns a concatenated String of 3 of the
+  // instance variables in a sentence like "..'s capital is .. and its primary
+  // language is ..."
+  public String toString() {
+    String combinedString;
+    combinedString = name + "'s capital is " + capital + " and the people there speak " + language;
+    return combinedString;
+  }
+
 }
--- workspace/Main.java
+++ workspace/Main.java
@@ -33,7 +33,9 @@ public class Main
     File file = new File("/workspaces/Countries/workspace/countries-data.csv");
     
     //create a scanner and a loop to read from the file until you've read everything.
+    Scanner scan = new Scanner(/* unsure of what goes here*/);
     // inside the loop you'll need to read in a line from the file and use "split" to break up the data into destinct parts.
+    
     // create a new Country using your constructor with 4 arguments (each of the arguments is a different part of the line you've read in)
     // inside the loop, set countryArray[i] to the created Country object
     //after running this method your array should contain all 10 countries from inside the countries-data file.
